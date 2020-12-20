c85b367c78502ff905e79466869f1bf4c09e63fa
OllieRumbol <otgb97@gmail.com>
2020-12-20 15:58:43 +0000

    Add project files.

---
+++ TreeCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
