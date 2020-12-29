ae581b0ac01adb8c6a7d819aa434b72c96adddd4
OllieRumbol <otgb97@gmail.com>
2020-12-29 17:36:58 +0000

    First working idea of break down equation into a tree

--- TreeCalculator/Program.cs
+++ TreeCalculator/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TreeCalculator
 {
     class Program
     {
+        public static Tree EquationToTree(string equation)
+        {
+            string[] brokenDown = Split(equation);
+            Tree tree = EquationToTreeWorker(brokenDown);
+
+            return tree;
+        }
+
+        private static string[] Split(string equation)
+        {
+            string[] result = new string[equation.Length];
+
+            for(int i = 0; i < equation.Length; i++){
+                result[i] = equation[i].ToString();
+            }
+
+            return result;
+        }
+
+        public static Tree EquationToTreeWorker(string[] equation)
+        {
+            if (equation.Length == 1)
+            {
+                return new Tree(equation[0]);
+            }
+
+            WeakestCalculationSymbol weakestCalculationSymbol = WeakestCalculationSymbol.fromCalculation(equation);
+
+            if (weakestCalculationSymbol.Symbol == string.Empty)
+            {
+                throw new Exception();
+            }
+
+            Tree workingTree = new Tree(weakestCalculationSymbol.Symbol);
+            string[] left = equation.Take(weakestCalculationSymbol.index).ToArray();
+            string[] right = equation.Skip(weakestCalculationSymbol.index + 1).Take(weakestCalculationSymbol.index).ToArray();
+
+            Tree.JoinLeft(workingTree, EquationToTreeWorker(left));
+            Tree.JoinRight(workingTree, EquationToTreeWorker(right));
+
+            return workingTree;
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start");
+            string equation = "5+3";
+            Tree tree = EquationToTree(equation);
+            Tree.Print(tree);
+
+            Console.WriteLine("End");
         }
     }
 }
---
+++ TreeCalculator/Tree.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeCalculator
+{
+    public class Tree
+    {
+        public string Node;
+        public Tree Left;
+        public Tree Right;
+
+        public Tree(string value)
+        {
+            this.Node = value;
+            this.Left = null;
+            this.Right = null;
+        }
+
+        public static int Size(Tree tree)
+        {
+            if (tree.Left != null)
+            {
+                return 1 + Size(tree.Left);
+            }
+            else if (tree.Right != null)
+            {
+                return 1 + Size(tree.Right);
+            }
+            return 1;
+        }
+
+        public static void Print(Tree tree)
+        {
+            int size = Size(tree);
+            for (int i = 1; i <= size; i++)
+            {
+                List<string> res = getRow(tree, 1, i, new List<string>());
+                foreach (string r in res)
+                {
+                    Console.Write(r);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static List<string> getRow(Tree tree, int counter, int goal, List<string> res)
+        {
+            if (counter == goal)
+            {
+                res.Add(tree.Node);
+            }
+            else
+            {
+                if (tree.Left != null)
+                {
+                    getRow(tree.Left, counter + 1, goal, res);
+                }
+                if (tree.Right != null)
+                {
+                    getRow(tree.Right, counter + 1, goal, res);
+                }
+            }
+            return res;
+        }
+
+        internal static Tree JoinLeft(Tree tree, Tree newTree)
+        {
+            if (tree == null)
+            {
+                return newTree;
+            }
+
+            tree.Left = newTree;
+            return tree;
+        }
+
+        internal static Tree JoinRight(Tree tree, Tree newTree)
+        {
+            if (tree == null)
+            {
+                return newTree;
+            }
+
+            tree.Right = newTree;
+            return tree;
+        }
+    }
+
+    public class WeakestCalculationSymbol
+    {
+        public string Symbol { get; set; }
+
+        public int index { get; set; }
+
+        public static WeakestCalculationSymbol fromCalculation(string[] equation)
+        {
+            for (int i = 0; i < equation.Length; i++)
+            {
+                string value = equation[i];
+                if (value == "-")
+                {
+                    return new WeakestCalculationSymbol
+                    {
+                        Symbol = value,
+                        index = i
+                    };
+                }
+            }
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                string value = equation[i];
+                if (value == "+")
+                {
+                    return new WeakestCalculationSymbol
+                    {
+                        Symbol = value,
+                        index = i
+                    };
+                }
+            }
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                string value = equation[i];
+                if (value == "*")
+                {
+                    return new WeakestCalculationSymbol
+                    {
+                        Symbol = value,
+                        index = i
+                    };
+                }
+            }
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                string value = equation[i];
+                if (value == "/")
+                {
+                    return new WeakestCalculationSymbol
+                    {
+                        Symbol = value,
+                        index = i
+                    };
+                }
+            }
+
+            return new WeakestCalculationSymbol
+            {
+                Symbol = string.Empty,
+                index = 0
+            };
+        }
+    }
+}
